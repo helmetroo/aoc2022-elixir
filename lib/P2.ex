3ccb74c202391d45f773b8340ce7110dc4e35455
Samuel Sweet <samueljsweet@gmail.com>
2022-12-02 13:24:13 -0700

    Solution for P2 part 2

--- lib/P2.ex
+++ lib/P2.ex
@@ -4,7 +4,8 @@ defmodule AOC2022.P2 do
   def get_puzzle_info do
     %Puzzle{
       number: 2,
-      part_one_mode: :full
+      part_one_mode: :full,
+      part_two_mode: :full
     }
   end
 
@@ -56,11 +57,55 @@ defmodule AOC2022.P2 do
 
   def round_score(round), do: shape_score(round) + outcome_score(round)
 
+  def with_chosen_move(round) do
+    chosen_move =
+      case round.your_move do
+        "X" -> choose_losing_move(round.opponent_move)
+        "Y" -> choose_draw_move(round.opponent_move)
+        "Z" -> choose_winning_move(round.opponent_move)
+        _ -> "Invalid round end"
+      end
+
+    %Round{round | your_move: chosen_move}
+  end
+
+  def choose_draw_move(move) do
+    case move do
+      "A" -> "X"
+      "B" -> "Y"
+      "C" -> "Z"
+      _ -> raise "Invalid opponent move"
+    end
+  end
+
+  def choose_losing_move(move) do
+    case move do
+      "A" -> "Z"
+      "B" -> "X"
+      "C" -> "Y"
+      _ -> raise "Invalid opponent move"
+    end
+  end
+
+  def choose_winning_move(move) do
+    case move do
+      "A" -> "Y"
+      "B" -> "Z"
+      "C" -> "X"
+      _ -> raise "Invalid opponent move"
+    end
+  end
+
   def solve_part_one(rounds) do
     rounds
     |> Enum.reduce(0, fn round, acc -> acc + round_score(round) end)
   end
 
   def solve_part_two(rounds) do
+    rounds
+    |> Enum.reduce(0, fn round, acc ->
+      updated_round_score = with_chosen_move(round) |> round_score()
+      acc + updated_round_score
+    end)
   end
 end
