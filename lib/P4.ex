09c82f4252afddcb478c9065ae5dc4858968f305
Samuel Sweet <samueljsweet@gmail.com>
2022-12-04 16:00:30 -0700

    Solution for P4 part 1

---
+++ lib/P4.ex
@@ -0,0 +1,60 @@
+defmodule AOC2022.P4 do
+  alias AOC2022.Puzzle, as: Puzzle
+
+  def get_puzzle_info do
+    %Puzzle{
+      number: 4,
+      part_one_mode: :full
+    }
+  end
+
+  defmodule RangePair do
+    @enforce_keys [:first, :second]
+    defstruct first: 0..0, second: 0..0
+
+    def contains_other(%RangePair{first: first, second: second}) do
+      first_size = Range.size(first)
+      second_size = Range.size(second)
+
+      {larger, smaller} =
+        if first_size >= second_size do
+          {first, second}
+        else
+          {second, first}
+        end
+
+      smaller.first >= larger.first &&
+        smaller.last <= larger.last
+    end
+  end
+
+  def parse_input_file(buffer) do
+    buffer
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_range_pair/1)
+  end
+
+  def parse_range_pair(range_pair_str) do
+    range_pair_pattern = ~r/(?<p1_s>\d*)-(?<p1_e>\d*),(?<p2_s>\d*)-(?<p2_e>\d*)/
+
+    %{
+      "p1_s" => p1_start,
+      "p1_e" => p1_end,
+      "p2_s" => p2_start,
+      "p2_e" => p2_end
+    } = Regex.named_captures(range_pair_pattern, range_pair_str)
+
+    pair_one = String.to_integer(p1_start)..String.to_integer(p1_end)
+    pair_two = String.to_integer(p2_start)..String.to_integer(p2_end)
+    %RangePair{first: pair_one, second: pair_two}
+  end
+
+  def solve_part_one(range_pairs) do
+    Enum.reduce(range_pairs, 0, fn pair, acc ->
+      if RangePair.contains_other(pair), do: acc + 1, else: acc
+    end)
+  end
+
+  def solve_part_two(_range_pairs) do
+  end
+end
--- lib/runner.ex
+++ lib/runner.ex
@@ -1,9 +1,9 @@
 defmodule AOC2022.Runner do
   alias AOC2022.Puzzle, as: Puzzle
-  alias AOC2022.P3, as: P3
+  alias AOC2022.P4, as: P4
 
   defp print_latest_solution do
-    solution = Puzzle.solve(P3.get_puzzle_info(), P3)
+    solution = Puzzle.solve(P4.get_puzzle_info(), P4)
 
     unless solution[:part_one] == nil do
       IO.puts("Part one answer: #{solution[:part_one]}")
