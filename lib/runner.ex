c8678e37cd3cdf5ca9d05dfa74e08431b8c564f4
Samuel Sweet <samueljsweet@gmail.com>
2022-12-06 17:53:09 -0700

    - Puzzle runner now only needs one argument - Solution printing now shows the input mode for each part

--- lib/runner.ex
+++ lib/runner.ex
@@ -3,14 +3,15 @@ defmodule AOC2022.Runner do
   alias AOC2022.P6, as: P6
 
   defp print_latest_solution do
-    solution = Puzzle.solve(P6.get_puzzle_info(), P6)
+    puzzle_info = P6.get_puzzle_info()
+    solution = Puzzle.solve(P6)
 
     unless solution[:part_one] == nil do
-      IO.puts("Part one answer: #{solution[:part_one]}")
+      IO.puts("Part one (#{puzzle_info.part_one_mode}) answer: #{solution[:part_one]}")
     end
 
     unless solution[:part_two] == nil do
-      IO.puts("Part two answer: #{solution[:part_two]}")
+      IO.puts("Part two (#{puzzle_info.part_two_mode}) answer: #{solution[:part_two]}")
     end
   end
 
